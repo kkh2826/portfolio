cc021975c429abc66627736fa044a642c80f2940
kyounghoon <kkh2826@naver.com>
2022-06-05 15:59:33 +0900

    Add gitignore and About/Header Fixed Position

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import Voxel from "./Voxel";
 
 function About() {
   return (
-    <Container p="0" maxW="1600px" h="90vh" mt="2rem" id="About">
+    <Container p="0" maxW="1600px" h="100vh" mt="2rem" id="About">
       <Flex
         h="100%"
         alignItems="center"
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,21 @@
-import { Container, Flex } from "@chakra-ui/react"
-import HeaderLogo from "./HeaderLogo"
-import Navigation from "./Navigation"
+import { Container, Flex } from "@chakra-ui/react";
+import HeaderLogo from "./HeaderLogo";
+import Navigation from "./Navigation";
 
 function Header() {
   return (
-    <Container maxW={"4xl"}>
+    <Container
+      maxW={"4xl"}
+      position="fixed"
+      left="50%"
+      transform="translate(-50%, -50%)"
+    >
       <Flex alignItems={"center"} justifyContent={"space-between"}>
         <HeaderLogo />
         <Navigation />
       </Flex>
     </Container>
-  )
+  );
 }
 
-export default Header
+export default Header;
